d431043c1168e517d7ee826afa4109f18f213920
dkfmlak <153356481+dkfmlak@users.noreply.github.com>
2025-10-20 19:32:39 +0900

    무신 스킬파라미터 초기화 수정

--- Assets/Scripts/EffectManager.cs
+++ Assets/Scripts/EffectManager.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
+using UnityEngine.Animations;
+
 
 public class EffectManager : MonoBehaviour
 {
+
+    [HideInInspector]public Animator anim;
+
+    private void Start()
+    {
+        anim = PlayerState.instance.anim;
+    }
     public void EffectOff()
     {
         gameObject.SetActive(false);
+        anim.SetInteger("AttackSkill", 0);
+
+
     }
 
 }
